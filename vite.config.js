93a7df29612c40d638f68edd06308f9c2a260042
one0910 <one0910.yu@msa.hinet.net>
2024-01-09 15:46:06 +0800

    增加環境變數及修改vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'vue-homework-week01',
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  // 加載環境變數
+  const env = loadEnv(mode, process.cwd(), '')
+  return defineConfig({
+    base: (env.NODE_ENV === 'development') ? '/' : env.VITE_APP_BASE_URL,
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  });
+};
